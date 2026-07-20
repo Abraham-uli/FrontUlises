ba20b880ab4a96d0530701879e5467c7f298d80c
Abraham Ortiz S <uaortizs@truper.com>
2026-07-20 07:56:17 -0600

    Copia de Front_CD_Imports

--- src/Componentes/Vistas_Onboarding/Menu_onboarding.js
+++ src/Componentes/Vistas_Onboarding/Menu_onboarding.js
@@ -5,7 +5,7 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { styled } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
-import image from './herramientasOK.jpg';
+import image from './Logo_Truper.jpg';
 import { Stack } from 'react-bootstrap';
 import styles from "./Administrador_Documentos.module.css";
 
@@ -22,25 +22,18 @@ function Menu_onboarding() {
             return "¡Buenas Noches!"          
         }   
     }
-    return (
-<div style={{ position: 'relative',marginLeft:'-15%' , width:'130%' , minHeight: '15vh' ,backgroundColor:"#444343" }}>
+return (
+<div style={{ position: 'relative',marginLeft:'-20%' , width:'130%' , minHeight: '15vh'  }}>
     <div style={{ position: 'absolute',
-        top: 0, left: 0,width: '100%', height: '100%', opacity: 0.3,backgroundImage: `url(${image})`, backgroundSize: 'cover',
-        backgroundPosition: 'center', backgroundRepeat: 'no-repeat', zIndex: 0 }}    />
+        top: 0, left: 0,width: '100%', height: '100%',backgroundImage: `url(${image})`, imgStyle: { objectFit: 'cover' }, backgroundSize: 'cover',
+        backgroundPosition: 'center', backgroundRepeat: 'repeat', zIndex: 0 }}    />
     <div style={{ position: 'relative', zIndex: 1 }}>;
-        {/* <div  className="dropdown">
-            <button className="btn btn-light" type="button" data-bs-toggle="dropdown" aria-expanded="false" style={{fontSize:'20px',color:"#f07027ff" , backgroundColor:'lightgray' , marginLeft:'5%' }} >
-                <b>Menu </b>
-            </button>
-            <ul className="dropdown-menu">
-                <li><Link to="/importaciones/AdmonDocs" style={{color:'#FF6400'}} className='dropdown-item'><strong>Documentos</strong></Link></li>
-                <li><Link to="/importaciones/Sesiones" style={{color:'#FF6400'}} className='dropdown-item'><strong>Sesiones</strong></Link></li>
-            </ul>
-        </div> */}
         <Stack direction='horizontal'  >
-            <h2 style={{marginLeft:'40%', color:'#FF6400', fontSize:'30px' , fontFamily:'serif'}} > <b>{momentodia()}</b></h2> 
+            <h2 style={{marginLeft:'30%', color:'#FF6400', fontSize:'70px', textAlign:'center', fontFamily:'serif'}} > <b>{momentodia()}</b></h2> 
                 <input style={{width:'20%' , marginLeft:'10%'}} className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
         </Stack>
+
+
         </div>
             <span className={styles.mower} style={{ marginLeft: "30%", fontSize: "40px" }}>
                 🚜  🔨 🦼
